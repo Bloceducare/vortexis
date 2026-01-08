0ba06cab2c487f1ee11ce63536ccf878f204376f
Amiola Demilade <amiolademilade@gmail.com>
2026-01-08 20:44:36 +0100

    fixed

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,22 +1,28 @@
-import { QueryClient, QueryClientProvider, HydrationBoundary } from "@tanstack/react-query";
-import { useState } from "react";
+'use client'
 
+import {
+  QueryClient,
+  QueryClientProvider,
+  HydrationBoundary,
+} from '@tanstack/react-query'
+import type { DehydratedState } from '@tanstack/react-query'
+import { useState } from 'react'
 
 export function Provider({
-    children,
-    dehydratedState,
-  }: {
-    children: React.ReactNode;
-    dehydratedState?: unknown;
-  }) {g
-    const [queryClient] = useState(() => new QueryClient());
-  
-    return (
-      <QueryClientProvider client={queryClient}>
-        <HydrationBoundary state={dehydratedState}>
-          {children}
-        </HydrationBoundary>
-      </QueryClientProvider>
-    );
+  children,
+  dehydratedState,
+}: {
+  children: React.ReactNode
+  dehydratedState?: DehydratedState
+}) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={dehydratedState}>
+        {children}
+      </HydrationBoundary>
+    </QueryClientProvider>
+  )
 }
-  
+
